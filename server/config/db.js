6d835cd6dedbf01ccf6d16d568faeb909dbff03b
unknown <anasmalik722422@gmail.com>
2024-11-13 22:57:22 -0700

    Initial commit

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DB_LOCAL_URI || "mongodb://127.0.0.1:27017/DepartmentStore");
+    await mongoose.connect(process.env.DB_LOCAL_URI || "mongodb://127.0.0.1:27017/DepartmentStore" );
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
